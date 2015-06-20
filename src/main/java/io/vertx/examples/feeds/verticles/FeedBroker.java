f6a45847ad1b134aebfb716a80c140587b275895
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-06-20 02:20:18 +0200

    refactor

--- src/main/java/io/vertx/examples/feeds/verticles/FeedBroker.java
+++ src/main/java/io/vertx/examples/feeds/verticles/FeedBroker.java
@@ -4,6 +4,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpHeaders;
@@ -20,8 +21,8 @@ import io.vertx.redis.RedisClient;
 import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
@@ -32,10 +33,10 @@ public class FeedBroker extends AbstractVerticle {
 	// private final static Long POLL_PERIOD = 3600000l;
 	private final static Long POLL_PERIOD = 10000l;
 	private final static Logger log = LoggerFactory.getLogger(FeedBroker.class);
+
+	private JsonObject config;
 	private MongoClient mongo;
 	private RedisDAO redis;
-	private JsonObject config;
-
 	private Long timerId;
 
 	@Override
@@ -62,33 +63,36 @@ public class FeedBroker extends AbstractVerticle {
 	}
 
 	private void readFeeds() {
-		mongo.find("feeds", new JsonObject(), result -> {
+		JsonObject crit = new JsonObject();
+		JsonObject gt0 = new JsonObject();
+		gt0.put("$gt", 0);
+		crit.put("subscriber_count", gt0);
+		mongo.find("feeds", crit, result -> {
 			if (result.failed()) {
 				log.error("Could not retrieve feed list from Mongo", result.cause());
 			} else {
-				List<JsonObject> allFeeds = result.result();
-				List<JsonObject> feeds = allFeeds.stream().filter(feed -> {
-					return feed.getInteger("subscriber_count") > 0;
-				}).collect(Collectors.toList());
-				MultipleFutures fetchFeedsFuture = new MultipleFutures();
-				feeds.forEach(feed -> {
-					fetchFeedsFuture.add(feedFuture -> {
-						readFeed(feed, feedFuture);
-					});
-				});
-				fetchFeedsFuture.setHandler(fetchResult -> {
-					// No matter if failed or succeeded -> re-launch
-					// periodically
-						timerId = vertx.setTimer(POLL_PERIOD, timerId -> {
-							readFeeds();
-						});
-					});
-				fetchFeedsFuture.start();
+				this.readFeeds(result.result());
 			}
 		});
 	}
 
-	public void readFeed(JsonObject jsonFeed, Future<Void> future) {
+	private void readFeeds(List<JsonObject> feeds) {
+		MultipleFutures fetchFeedsFuture = new MultipleFutures();
+		feeds.forEach(feed -> {
+			fetchFeedsFuture.add(feedFuture -> {
+				readFeed(feed, feedFuture);
+			});
+		});
+		fetchFeedsFuture.setHandler(fetchResult -> {
+			/* No matter if failed or succeeded -> re-launch periodically */
+			timerId = vertx.setTimer(POLL_PERIOD, timerId -> {
+				readFeeds();
+			});
+		});
+		fetchFeedsFuture.start();
+	}
+
+	private void readFeed(JsonObject jsonFeed, Future<Void> future) {
 		String feedUrl = jsonFeed.getString("url");
 		String feedId = jsonFeed.getString("hash");
 		URL url;
@@ -96,16 +100,14 @@ public class FeedBroker extends AbstractVerticle {
 			url = new URL(feedUrl);
 		} catch (MalformedURLException mfe) {
 			log.warn("Invalid url : " + feedUrl, mfe);
-			if (future != null) {
-				future.fail(mfe);
-			}
+			future.fail(mfe);
 			return;
 		}
 
 		redis.getMaxDate(feedId, maxDate -> {
 			HttpClient client = createClient(url);
 			client.get(url.getPath(), response -> {
-				final int status = response.statusCode();
+				int status = response.statusCode();
 				if (status < 200 || status >= 300) {
 					if (future != null) {
 						future.fail(new RuntimeException("Could not read feed " + feedUrl + ". Response status code : " + status));
@@ -113,36 +115,40 @@ public class FeedBroker extends AbstractVerticle {
 					return;
 				}
 				response.bodyHandler(buffer -> {
-					String xmlFeed = buffer.toString("UTF-8");
-					StringReader xmlReader = new StringReader(xmlFeed);
-					SyndFeedInput feedInput = new SyndFeedInput();
-					try {
-						SyndFeed feed = feedInput.build(xmlReader);
-						JsonObject feedJson = FeedUtils.toJson(feed);
-						log.info(feedJson);
-						List<JsonObject> jsonEntries = FeedUtils.toJson(feed.getEntries(), maxDate);
-						log.info("Insert " + jsonEntries.size() + " entries into Redis");
-						if (jsonEntries.size() == 0) {
-							future.complete();
-							return;
-						}
-						vertx.eventBus().publish(feedId, new JsonArray(jsonEntries));
-						redis.insertEntries(feedId, jsonEntries, handler -> {
-							if (handler.failed()) {
-								log.error("Insert failed", handler.cause());
-								future.fail(handler.cause());
-							} else {
-								future.complete();
-							}
-						});
-					} catch (FeedException fe) {
-						log.error("Exception while reading feed : " + url.toString(), fe);
-						future.fail(fe);
-						return;
-					}
+					this.parseXmlFeed(buffer, maxDate, url, feedId, future);
 				});
 			}).putHeader(HttpHeaders.ACCEPT, "application/xml").end();
 		});
+	}
+
+	private void parseXmlFeed(Buffer buffer, Date maxDate, URL url, String feedId, Future<Void> future) {
+		String xmlFeed = buffer.toString("UTF-8");
+		StringReader xmlReader = new StringReader(xmlFeed);
+		SyndFeedInput feedInput = new SyndFeedInput();
+		try {
+			SyndFeed feed = feedInput.build(xmlReader);
+			JsonObject feedJson = FeedUtils.toJson(feed);
+			log.info(feedJson);
+			List<JsonObject> jsonEntries = FeedUtils.toJson(feed.getEntries(), maxDate);
+			log.info("Insert " + jsonEntries.size() + " entries into Redis");
+			if (jsonEntries.size() == 0) {
+				future.complete();
+				return;
+			}
+			vertx.eventBus().publish(feedId, new JsonArray(jsonEntries));
+			redis.insertEntries(feedId, jsonEntries, handler -> {
+				if (handler.failed()) {
+					log.error("Insert failed", handler.cause());
+					future.fail(handler.cause());
+				} else {
+					future.complete();
+				}
+			});
+		} catch (FeedException fe) {
+			log.error("Exception while reading feed : " + url.toString(), fe);
+			future.fail(fe);
+			return;
+		}
 	}
 
 	private HttpClient createClient(URL url) {
